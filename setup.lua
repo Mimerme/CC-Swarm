f6692fbedb30d29cdd78bbc252e145f883173a27
Andros Yang <androssdragonq.9@gmail.com>
2020-11-24 16:19:21 -0500

    updated setup

--- setup.lua
+++ setup.lua
@@ -1,17 +1,24 @@
 print("Downloading scripts from pastebin...")
 print("MrMrMr Obama")
 
+print("Removing all programs")
+shell.run("delete sethome")
+shell.run("delete home")
+shell.run("delete goto")
+shell.run("delete swarm_slave")
+shell.run("delete swarm_master")
+
 print("Downloading sethome")
-shell.run("https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/sethome.lua")
+shell.run("wget https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/sethome.lua")
 
 print("Downloading home")
-shell.run("https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/home.lua")
+shell.run("wget https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/home.lua")
 
 print("Downloading goto")
-shell.run("https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/goto.lua")
+shell.run("wget https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/goto.lua")
 
 print("Downloading slave")
-shell.run("https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/swarm_slave.lua")
+shell.run("wget https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/swarm_slave.lua")
 
 print("Downloading master")
-shell.run("https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/swarm_master.lua")
+shell.run("wget https://raw.githubusercontent.com/Mimerme/CC-Swarm/main/swarm_master.lua")
